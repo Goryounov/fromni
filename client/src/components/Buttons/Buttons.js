7d316e7063e4972fa8b14a3a2cf0c4f932681630
Goryounov <nikita@goryunoff.ru>
2022-07-17 17:04:20 +0300

    removed unused imports

--- client/src/components/Buttons/Buttons.js
+++ client/src/components/Buttons/Buttons.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
 import Errors from "../Errors/Errors";
--- client/src/hooks/useValidation.js
+++ client/src/hooks/useValidation.js
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect} from "react";
 import {Context} from "../index";
 
 const useValidation = (value, name, validations) => {
